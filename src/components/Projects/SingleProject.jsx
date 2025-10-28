6456a3e512727d8cfd067e0b8b10cb07f151df3d
mohammadSadegh <mv818179@gmail.com>
2025-10-28 23:04:56 +0330

    update SingleProject.jsx and SingleProject.css

--- src/components/Projects/SingleProject.jsx
+++ src/components/Projects/SingleProject.jsx
@@ -1,35 +1,43 @@
 import "./SingleProject.css";
-import { GoArrowUpRight } from "react-icons/go";
-import { FaStar } from "react-icons/fa";
-import { useState } from "react";
+import { FaGithubSquare } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
+import { MdFavorite } from "react-icons/md";
 
 const SingleProject = (props) => {
-    const { category, favorite, image, titleProject, url } = props;
-    const [show, setShow] = useState(false);
+    const {
+        skills,
+        favorite,
+        image,
+        titleProject,
+        websiteURL,
+        githubURL,
+        description,
+    } = props;
     return (
-        <article
-            className="single-project"
-            onMouseEnter={() => setShow(true)}
-            onMouseLeave={() => setShow(false)}
-        >
+        <article className="single-project">
             <img src={image} alt={titleProject} />
-            <span className="bookmark">{category.join(" , ")}</span>
-            {favorite ? (
-                <span className="favorite">
-                    <FaStar />
-                </span>
-            ) : (
-                ""
-            )}
-            <section
-                className={
-                    show ? "single-project-info show" : "single-project-info"
-                }
-            >
-                <a href={url} target="_blank">
-                    {titleProject}
-                    <GoArrowUpRight />
-                </a>
+            <section>
+                <h3>{titleProject}</h3>
+                <p>{description}</p>
+                <ul>
+                    <li>
+                        <a href={websiteURL}>
+                            <TbWorldWww />
+                        </a>
+                    </li>
+                    <li>
+                        <a href={githubURL}>
+                            <FaGithubSquare />
+                        </a>
+                    </li>
+                    {favorite ? (
+                        <li>
+                            <MdFavorite />
+                        </li>
+                    ) : (
+                        ""
+                    )}
+                </ul>
             </section>
         </article>
     );
